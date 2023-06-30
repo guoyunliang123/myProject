60d36e76610b4d7737923fb8ebe42c9139a847d6
郭云亮 <v-guoyunliang@sinochem.com>
2023-06-30 09:20:04 +0800

    feat：leetcode：500 - 键盘行

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -323,6 +323,89 @@ const nextGreaterElement = (nums1, nums2) => {
     }
     return arr
 }
-console.log(nextGreaterElement([4, 1, 2], [1, 3, 4, 2]))
+// console.log(nextGreaterElement([4, 1, 2], [1, 3, 4, 2]))
 // console.log(nextGreaterElement([2,4], [1, 2, 3, 4]))
 // console.log(nextGreaterElement([1,3,5,2,4], [6,5,4,3,2,1,7]))
+
+/**
+ * 500. 键盘行
+ *
+ * 给你一个字符串数组 words ，只返回可以使用在 美式键盘 同一行的字母打印出来的单词。键盘如下图所示。
+ *
+ * 美式键盘 中：
+ *
+ * 第一行由字符 "qwertyuiop" 组成。
+ * 第二行由字符 "asdfghjkl" 组成。
+ * 第三行由字符 "zxcvbnm" 组成。
+ *
+ * 示例 1：
+ * 输入：words = ["Hello","Alaska","Dad","Peace"]
+ * 输出：["Alaska","Dad"]
+ *
+ * 示例 2：
+ * 输入：words = ["omk"]
+ * 输出：[]
+ *
+ * 示例 3：
+ * 输入：words = ["adsdf","sfd"]
+ * 输出：["adsdf","sfd"]
+ *
+ * 提示：
+ * 1 <= words.length <= 20
+ * 1 <= words[i].length <= 100
+ * words[i] 由英文字母（小写和大写字母）组成
+ * */
+const findWords = (words) => {
+    // 方法1：
+
+    // 如果正则使用 /gi 则会导致错误
+    // let len1 = /[qwertyuiop]/gi;
+    // let len2 = /[asdfghjkl]/gi;
+    // let len3 = /[zxcvbnm]/gi;
+
+    // var reg = /a/g; 对于使用 /gi 的注解
+    // console.log(reg.test('aa')); // true
+    // console.log(reg.lastIndex); // 1
+    // console.log(reg.test('aa')); // true
+    // console.log(reg.lastIndex); // 2
+    // console.log(reg.test('aa')); // false
+    // console.log(reg.lastIndex); // 0
+
+    // let res = []
+    // let len1 = /[qwertyuiop]/i;
+    // let len2 = /[asdfghjkl]/i;
+    // let len3 = /[zxcvbnm]/i;
+    // for (let item of words) {
+    //     let count1 = len1.test(item)
+    //     let count2 = len2.test(item)
+    //     let count3 = len3.test(item)
+    //     if ((count1 + count2 + count3) === 1) {
+    //         res.push(item)
+    //     }
+    // }
+    // return res
+
+    let arr = [];
+    words.forEach(item => {
+        //  将原有数组中的每个字符串去重组成新数组
+        let item2 = [...new Set(item.toLowerCase().split(''))];
+        /**
+         * item2: ['h', 'e', 'l', 'o']
+         * ['a', 'l', 's', 'k']
+         * */
+        if (test(item2)) {
+            arr.push(item);
+        }
+    });
+    function test(item2Arr) {
+        let one = item2Arr[0];
+        let str = '';
+        // 先判断每组字符在键盘的哪一行，然后在判断其余字符是否也存在该行中
+        str = 'qwertyuiop'.includes(one) ? 'qwertyuiop' : 'asdfghjkl'.includes(one) ? 'asdfghjkl' : 'zxcvbnm';
+        // 最后判断获取的过滤后的数组长度是否和传入的 item2Arr 长度相等
+        return item2Arr.filter(item => str.includes(item)).length === item2Arr.length;
+    }
+    return arr;
+
+}
+// console.log(findWords(["Hello", "Alaska", "Dad", "Peace"]))
