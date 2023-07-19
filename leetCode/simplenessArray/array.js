fbed4bed11f8fbd4afa29304e17b0e9f490ede15
郭云亮 <v-guoyunliang@sinochem.com>
2023-07-19 15:37:13 +0800

    feat：leetcode：594 - 最长和谐子序列

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -859,3 +859,68 @@ const findLengthOfLCIS = (nums) => {
     return result;
 }
 // console.log(findLengthOfLCIS([1,3,5,4,7]))
+
+/**
+ * 594. 最长和谐子序列
+ *
+ * 和谐数组是指一个数组里元素的最大值和最小值之间的差别 正好是 1 。
+ *
+ * 现在，给你一个整数数组 nums ，请你在所有可能的子序列中找到最长的和谐子序列的长度。
+ *
+ * 数组的子序列是一个由数组派生出来的序列，它可以通过删除一些元素或不删除元素、且不改变其余元素的顺序而得到。
+ *
+ * 示例 1：
+ * 输入：nums = [1,3,2,2,5,2,3,7]
+ * 输出：5
+ * 解释：最长的和谐子序列是 [3,2,2,2,3]
+ *
+ * 示例 2：
+ * 输入：nums = [1,2,3,4]
+ * 输出：2
+ *
+ * 示例 3：
+ * 输入：nums = [1,1,1,1]
+ * 输出：0
+ *
+ * 提示：
+ * 1 <= nums.length <= 2 * 104
+ * -109 <= nums[i] <= 109
+ * */
+const findLHS = (nums) => {
+    // 方法1
+    // 思路：先确认 nums 数组中每个元素出现的次数，然后在判断每个元素 -1 或者 +1 的值是否存在，存在的话就判断
+    // 当前元素出现次数 + 当前元素 -1 出现的次数合 和 当前元素出现次数 + 当前元素 +1 出现的次数合，获取最大的值输出
+
+    // let map = new Map();
+    // for(let i of nums) {
+    //     if(!map.has(i)) {
+    //         map.set(i, 1);
+    //     } else {
+    //         map.set(i, map.get(i) + 1)
+    //     }
+    // }
+    // let res = 0;
+    // // 循环 map.keys(), 判断 map.keys() 中每个元素 -1 的值或者 +1 的值是否存在，存在的话获取 -1 的值出现的次数和当前元素出现的次数和
+    // // 或者 +1 的值出现的次数和当前元素出现的次数和 得出最大值则为结果
+    // for (let key of map.keys()) {
+    //     if(map.has(key - 1)) {
+    //         res = Math.max(res, map.get(key) + map.get(key - 1));
+    //     } else if(map.has(key + 1)) {
+    //         res = Math.max(res, map.get(key) + map.get(key + 1));
+    //     }
+    // }
+    // return res;
+
+    // 方法2：
+
+    nums.sort((a,b) => a - b);
+    let res = 0;
+    for (let i = 0, j = 0; j < nums.length; j++) {
+        while (i < j && nums[j] - nums[i] > 1) i++;
+        if(nums[j] - nums[i] === 1) {
+            res = Math.max(res, j - i + 1)
+        }
+    }
+    return res;
+}
+console.log(findLHS([1,3,2,2,5,2,3,7]))
