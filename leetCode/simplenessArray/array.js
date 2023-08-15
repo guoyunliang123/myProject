9a7cece56cee369643ab871f40cb84d7bcc0b59a
郭云亮 <1060950677@qq.com>
2023-08-15 10:14:14 +0800

    feat：leetcode：724 - 寻找数组的中心下标

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -1347,6 +1347,66 @@ const search = (nums, target) => {
 // console.log(search([-1,0,3,5,9,12], 9))
 
 /**
+ * 724. 寻找数组的中心下标
+ *
+ * 给你一个整数数组 nums ，请计算数组的 中心下标 。
+ *
+ * 数组 中心下标 是数组的一个下标，其左侧所有元素相加的和等于右侧所有元素相加的和。
+ *
+ * 如果中心下标位于数组最左端，那么左侧数之和视为 0 ，因为在下标的左侧不存在元素。这一点对于中心下标位于数组最右端同样适用。
+ *
+ * 如果数组有多个中心下标，应该返回 最靠近左边 的那一个。如果数组不存在中心下标，返回 -1 。
+ *
+ * 示例 1：
+ * 输入：nums = [1, 7, 3, 6, 5, 6]
+ * 输出：3
+ * 解释：
+ * 中心下标是 3 。
+ * 左侧数之和 sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11 ，
+ * 右侧数之和 sum = nums[4] + nums[5] = 5 + 6 = 11 ，二者相等。
+ *
+ * 示例 2：
+ * 输入：nums = [1, 2, 3]
+ * 输出：-1
+ * 解释：
+ * 数组中不存在满足此条件的中心下标。
+ *
+ * 示例 3：
+ * 输入：nums = [2, 1, -1]
+ * 输出：0
+ * 解释：
+ * 中心下标是 0 。
+ * 左侧数之和 sum = 0 ，（下标 0 左侧不存在元素），
+ * 右侧数之和 sum = nums[1] + nums[2] = 1 + -1 = 0 。
+ *
+ * 提示：
+ * 1 <= nums.length <= 104
+ * -1000 <= nums[i] <= 1000
+ * */
+const pivotIndex = (nums) => {
+    // 方法1：
+    // let left = 0, right = nums.reduce((a, b) => a + b, 0) - nums[0];
+    // if(right === 0) return 0;
+    // for (let i = 0; i < nums.length; i++) {
+    //     if(left === right) return i;
+    //     left += nums[i];
+    //     right -= nums[i + 1];
+    // }
+    // return -1;
+
+    // 方法2：思路一样，只是写法不同
+    let left = 0;
+    let right = nums.reduce((pre, num) => pre + num, 0)
+    for (let i = 0; i < nums.length; i++) {
+        right -= nums[i];
+        if(left === right) return i;
+        left += nums[i];
+    }
+    return -1;
+}
+console.log(pivotIndex([2, 1, -1]))
+
+/**
  * 746. 使用最小花费爬楼梯
  *
  * 给你一个整数数组 cost ，其中 cost[i] 是从楼梯第 i 个台阶向上爬需要支付的费用。一旦你支付此费用，即可选择向上爬一个或者两个台阶。
