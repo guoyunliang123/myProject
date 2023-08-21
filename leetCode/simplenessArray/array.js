2760a7fcbb02c20e8597e1ea38be5fd2752fc121
郭云亮 <1060950677@qq.com>
2023-08-21 16:40:43 +0800

    feat：leetcode：961 - 在长度 2N 的数组中找出重复 N 次的元素

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -1487,7 +1487,7 @@ const sortArrayByParityII = (nums) => {
     }
     return result;
 }
-console.log(sortArrayByParityII([4,2,5,7]))
+// console.log(sortArrayByParityII([4,2,5,7]))
 
 /**
  * 942. 增减字符串匹配
@@ -1523,6 +1523,47 @@ const diStringMatch = (s) => {
     return result;
 }
 // console.log(diStringMatch('IDID'))
+
+/**
+ * 961. 在长度 2N 的数组中找出重复 N 次的元素
+ *
+ * 给你一个整数数组 nums ，该数组具有以下属性：
+ *
+ * nums.length == 2 * n.
+ * nums 包含 n + 1 个 不同的 元素
+ * nums 中恰有一个元素重复 n 次
+ * 找出并返回重复了 n 次的那个元素。
+ *
+ * 示例 1：
+ * 输入：nums = [1,2,3,3]
+ * 输出：3
+ *
+ * 示例 2：
+ * 输入：nums = [2,1,2,5,3,2]
+ * 输出：2
+ *
+ * 示例 3：
+ * 输入：nums = [5,1,5,2,5,3,5,4]
+ * 输出：5
+ *
+ * 提示：
+ * 2 <= n <= 5000
+ * nums.length == 2 * n
+ * 0 <= nums[i] <= 104
+ * nums 由 n + 1 个 不同的 元素组成，且其中一个元素恰好重复 n 次
+ * */
+const repeatedNTimes = (nums) => {
+    let resultMap = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (resultMap.has(nums[i])) {
+            return nums[i]
+        } else {
+            resultMap.set(nums[i], 1)
+        }
+    }
+    return result;
+}
+console.log(repeatedNTimes([5,1,5,2,5,3,5,4]))
 
 /**
  * 977. 有序数组的平方
