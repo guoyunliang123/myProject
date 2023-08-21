b187bc36735664eb98fec7fbab9f14aa57b71301
郭云亮 <1060950677@qq.com>
2023-08-21 16:47:47 +0800

    feat：leetcode：905 - 按奇偶排序数组

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -1452,6 +1452,38 @@ const minCostClimbingStairs = (cost) => {
 // console.log(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
 
 /**
+ * 905. 按奇偶排序数组
+ *
+ * 给你一个整数数组 nums，将 nums 中的的所有偶数元素移动到数组的前面，后跟所有奇数元素。
+ * 返回满足此条件的 任一数组 作为答案。
+ *
+ * 示例 1：
+ * 输入：nums = [3,1,2,4]
+ * 输出：[2,4,3,1]
+ * 解释：[4,2,3,1]、[2,4,1,3] 和 [4,2,1,3] 也会被视作正确答案。
+ *
+ * 示例 2：
+ * 输入：nums = [0]
+ * 输出：[0]
+ *
+ * 提示：
+ * 1 <= nums.length <= 5000
+ * 0 <= nums[i] <= 5000
+ * */
+const sortArrayByParity = (nums) => {
+    let result = [];
+    for (let i = 0; i < nums.length; i++) {
+        if(nums[i] % 2 === 0) {
+            result.unshift(nums[i])
+        } else {
+            result.push(nums[i])
+        }
+    }
+    return result;
+}
+console.log(sortArrayByParity([3,1,2,4]))
+
+/**
  * 922. 按奇偶排序数组 II
  *
  * 给定一个非负整数数组 nums，  nums 中一半整数是 奇数 ，一半整数是 偶数 。
@@ -1563,7 +1595,7 @@ const repeatedNTimes = (nums) => {
     }
     return result;
 }
-console.log(repeatedNTimes([5,1,5,2,5,3,5,4]))
+// console.log(repeatedNTimes([5,1,5,2,5,3,5,4]))
 
 /**
  * 977. 有序数组的平方
