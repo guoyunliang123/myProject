b605846ef74841758967fa027b4a89368972ea89
郭云亮 <1060950677@qq.com>
2023-08-21 16:33:46 +0800

    feat：leetcode：922 - 按奇偶排序数组 II

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -1452,6 +1452,79 @@ const minCostClimbingStairs = (cost) => {
 // console.log(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
 
 /**
+ * 922. 按奇偶排序数组 II
+ *
+ * 给定一个非负整数数组 nums，  nums 中一半整数是 奇数 ，一半整数是 偶数 。
+ * 对数组进行排序，以便当 nums[i] 为奇数时，i 也是 奇数 ；当 nums[i] 为偶数时， i 也是 偶数 。
+ * 你可以返回 任何满足上述条件的数组作为答案 。
+ *
+ * 示例 1：
+ * 输入：nums = [4,2,5,7]
+ * 输出：[4,5,2,7]
+ * 解释：[4,7,2,5]，[2,5,4,7]，[2,7,4,5] 也会被接受。
+ *
+ * 示例 2：
+ * 输入：nums = [2,3]
+ * 输出：[2,3]
+ *
+ * 提示：
+ * 2 <= nums.length <= 2 * 104
+ * nums.length 是偶数
+ * nums 中一半是偶数
+ * 0 <= nums[i] <= 1000
+ * */
+const sortArrayByParityII = (nums) => {
+    let odd = [], even = [], result = [];
+    for (let i = 0; i < nums.length; i++) {
+        if(nums[i] % 2 === 0) {
+            even.push(nums[i])
+        } else {
+            odd.push(nums[i])
+        }
+    }
+    for (let i = 0; i < odd.length; i++) {
+        result.push(even[i], odd[i])
+    }
+    return result;
+}
+console.log(sortArrayByParityII([4,2,5,7]))
+
+/**
+ * 942. 增减字符串匹配
+ *
+ * 由范围 [0,n] 内所有整数组成的 n + 1 个整数的排列序列可以表示为长度为 n 的字符串 s ，其中:
+ * 如果 perm[i] < perm[i + 1] ，那么 s[i] == 'I'
+ * 如果 perm[i] > perm[i + 1] ，那么 s[i] == 'D'
+ * 给定一个字符串 s ，重构排列 perm 并返回它。如果有多个有效排列perm，则返回其中 任何一个 。
+ *
+ * 示例 1：
+ * 输入：s = "IDID"
+ * 输出：[0,4,1,3,2]
+ *
+ * 示例 2：
+ * 输入：s = "III"
+ * 输出：[0,1,2,3]
+ *
+ * 示例 3：
+ * 输入：s = "DDI"
+ * 输出：[3,2,0,1]
+ *
+ * 提示：
+ * 1 <= s.length <= 105
+ * s 只包含字符 "I" 或 "D"
+ * */
+const diStringMatch = (s) => {
+    let maxCount = s.length, minCount = 0;
+    let result = [];
+    for (let i = 0; i < s.length; i++) {
+        s[i] === 'I' ? result.push(minCount++) : result.push(maxCount--);
+    }
+    result[s.length - 1] === 'I' ? result.push(minCount) : result.push(maxCount)
+    return result;
+}
+// console.log(diStringMatch('IDID'))
+
+/**
  * 977. 有序数组的平方
  *
  * 给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
