18bd2daef3c2f758969ba2f784a8694051563cd3
郭云亮 <1060950677@qq.com>
2023-08-22 10:44:11 +0800

    feat：leetcode：804 - 唯一的摩尔斯密码词

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -1452,6 +1452,72 @@ const minCostClimbingStairs = (cost) => {
 // console.log(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
 
 /**
+ * 804. 唯一摩尔斯密码词
+ *
+ * 国际摩尔斯密码定义一种标准编码方式，将每个字母对应于一个由一系列点和短线组成的字符串， 比如:
+ * 'a' 对应 ".-" ，
+ * 'b' 对应 "-..." ，
+ * 'c' 对应 "-.-." ，以此类推。
+ *
+ * 给你一个字符串数组 words ，每个单词可以写成每个字母对应摩尔斯密码的组合。
+ * 例如，"cab" 可以写成 "-.-..--..." ，(即 "-.-." + ".-" + "-..." 字符串的结合)。我们将这样一个连接过程称作 单词翻译 。
+ * 对 words 中所有单词进行单词翻译，返回不同 单词翻译 的数量。
+ *
+ *
+ * 示例 1：
+ * 输入: words = ["gin", "zen", "gig", "msg"]
+ * 输出: 2
+ * 解释:
+ * 各单词翻译如下:
+ * "gin" -> "--...-."
+ * "zen" -> "--...-."
+ * "gig" -> "--...--."
+ * "msg" -> "--...--."
+ * 共有 2 种不同翻译, "--...-." 和 "--...--.".
+ *
+ * 示例 2：
+ * 输入：words = ["a"]
+ * 输出：1
+ *
+ * 提示：
+ * 1 <= words.length <= 100
+ * 1 <= words[i].length <= 12
+ * words[i] 由小写英文字母组成
+ * */
+const uniqueMorseRepresentations = (words) => {
+    let passWord = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+
+    // 方法1：
+    // let s = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    // let result = [];
+    // let map = new Map();
+    // for (let i = 0; i < passWord.length; i++) {
+    //     map.set(s[i], passWord[i])
+    // }
+    // for (let i = 0; i < words.length; i++) {
+    //     let key = words[i];
+    //     let s = ''
+    //     for (let i = 0; i < key.length; i++) {
+    //         s += map.get(key[i])
+    //     }
+    //     result.push(s)
+    // }
+    // return [...new Set(result)].length;
+
+    // 方法2
+    const seen = new Set();
+    for (const word of words) {
+        let code = '';
+        for (const ch of word) {
+            code += passWord[ch.charCodeAt() - 'a'.charCodeAt()];
+        }
+        seen.add(code);
+    }
+    return seen.size;
+}
+// console.log(uniqueMorseRepresentations(["gin", "zen", "gig", "msg"]))
+
+/**
  * 821. 字符的最短距离
  *
  * 给你一个字符串 s 和一个字符 c ，且 c 是 s 中出现过的字符。
