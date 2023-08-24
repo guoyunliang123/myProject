ce83704dc9c4a8ec4314c12fd2b795f056c38ede
郭云亮 <1060950677@qq.com>
2023-08-24 10:59:52 +0800

    feat：leetcode：812 - 最大三角形面积

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -1597,7 +1597,36 @@ const numberOfLines = (widths, s) =>{
     }
     return [line, count];
 }
-console.log(numberOfLines([4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10],  "bbbcccdddaaa"))
+// console.log(numberOfLines([4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10],  "bbbcccdddaaa"))
+
+/**
+ * 812. 最大三角形面积
+ *
+ * 给你一个由 X-Y 平面上的点组成的数组 points ，其中 points[i] = [xi, yi] 。从其中取任意三个不同的点组成三角形，返回能组成的最大三角形的面积。与真实值误差在 10-5 内的答案将会视为正确答案。
+ *
+ * 示例 1：
+ * 输入：points = [[0,0],[0,1],[1,0],[0,2],[2,0]]
+ * 输出：2.00000
+ * 解释：输入中的 5 个点如上图所示，红色的三角形面积最大。
+ *
+ * 示例 2：
+ * 输入：points = [[1,0],[0,0],[0,1]]
+ * 输出：0.50000
+ *
+ * 提示：
+ * 3 <= points.length <= 50
+ * -50 <= xi, yi <= 50
+ * 给出的所有点 互不相同
+ * */
+const largestTriangleArea = (points) => {
+    // 面积公式如图 img 文件夹下 812 图片
+    let max = 0;
+    points.map(i => points.map(j => points.map (k => {
+        max = Math.max(max, i[0] * (j[1] - k[1]) + j[0] * (k[1] - i[1]) + k[0] * (i[1] - j[1]))
+    })))
+    return max / 2;
+}
+console.log(largestTriangleArea([[4,6],[6,5],[3,1]]))
 
 /**
  * 821. 字符的最短距离
