adfcb9f1cb59563942118e75d1843a9818c45df3
郭云亮 <1060950677@qq.com>
2023-08-24 14:28:19 +0800

    feat：leetcode：888 - 公平的糖果交换

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -1626,7 +1626,7 @@ const largestTriangleArea = (points) => {
     })))
     return max / 2;
 }
-console.log(largestTriangleArea([[4,6],[6,5],[3,1]]))
+// console.log(largestTriangleArea([[4,6],[6,5],[3,1]]))
 
 /**
  * 821. 字符的最短距离
@@ -1747,6 +1747,65 @@ const lemonadeChange = (bills) => {
     return true;
 }
 // console.log(lemonadeChange([5,5,5,10,20]))
+
+/**
+ * 888. 公平的糖果交换
+ *
+ * 爱丽丝和鲍勃拥有不同总数量的糖果。给你两个数组 aliceSizes 和 bobSizes ，aliceSizes[i] 是爱丽丝拥有的第 i 盒糖果中的糖果数量，bobSizes[j] 是鲍勃拥有的第 j 盒糖果中的糖果数量。
+ *
+ * 两人想要互相交换一盒糖果，这样在交换之后，他们就可以拥有相同总数量的糖果。一个人拥有的糖果总数量是他们每盒糖果数量的总和。
+ *
+ * 返回一个整数数组 answer，其中 answer[0] 是爱丽丝必须交换的糖果盒中的糖果的数目，answer[1] 是鲍勃必须交换的糖果盒中的糖果的数目。如果存在多个答案，你可以返回其中 任何一个 。题目测试用例保证存在与输入对应的答案。
+ *
+ * 示例 1：
+ * 输入：aliceSizes = [1,1], bobSizes = [2,2]
+ * 输出：[1,2]
+ *
+ * 示例 2：
+ * 输入：aliceSizes = [1,2], bobSizes = [2,3]
+ * 输出：[1,2]
+ *
+ * 示例 3：
+ * 输入：aliceSizes = [2], bobSizes = [1,3]
+ * 输出：[2,3]
+ *
+ * 示例 4：
+ * 输入：aliceSizes = [1,2,5], bobSizes = [2,4]
+ * 输出：[5,4]
+ *
+ * 提示：
+ * 1 <= aliceSizes.length, bobSizes.length <= 104
+ * 1 <= aliceSizes[i], bobSizes[j] <= 105
+ * 爱丽丝和鲍勃的糖果总数量不同。
+ * 题目数据保证对于给定的输入至少存在一个有效答案。
+ * */
+const fairCandySwap = (aliceSizes, bobSizes) => {
+    // 方法1：
+    // let aCount = aliceSizes.reduce((x, y) => x + y, 0)
+    // let bCount = bobSizes.reduce((x, y) => x + y, 0)
+    // console.log(aCount, bCount)
+    // for (let i = 0; i < aliceSizes.length; i++) {
+    //     for (let j = 0; j < bobSizes.length; j++) {
+    //         if(aCount - aliceSizes[i] + bobSizes[j] === bCount - bobSizes[j] + aliceSizes[i]) {
+    //             return [aliceSizes[i], bobSizes[j]]
+    //         }
+    //     }
+    // }
+
+    // 方法2：
+    let sumAlice = aliceSizes.reduce((cur, acc) => cur + acc, 0);
+    let sumBob = bobSizes.reduce((cur, acc) => cur + acc, 0);
+    const target = (sumAlice - sumBob) / 2; // 获取两个数组的差值
+    const setB = new Set(bobSizes);
+    // 遍历 aliceSizes，找到一个 bobSizes 中存在的值，与 target 相减，如果 setB 中存在，则返回
+    for (let candy of aliceSizes) {
+        const complement = candy - target;
+        if(setB.has(complement)) {
+            return [candy, complement];
+        }
+    }
+}
+console.log(fairCandySwap([1, 2, 5], [2, 4]))
 
 /**
  * 905. 按奇偶排序数组
