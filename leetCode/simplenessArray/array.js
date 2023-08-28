44c4e38c5b5eaa39d9e69d2be17da39eac606bfe
郭云亮 <1060950677@qq.com>
2023-08-28 16:37:27 +0800

    feat：leetcode：989 - 数组形式的整数加法

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -2074,6 +2074,46 @@ const sortedSquares = (nums) => {
 // console.log(sortedSquares([-4,-1,0,3,10]))
 
 /**
+ * 989. 数组形式的整数加法
+ *
+ * 整数的 数组形式  num 是按照从左到右的顺序表示其数字的数组。
+ *
+ * 例如，对于 num = 1321 ，数组形式是 [1,3,2,1] 。
+ * 给定 num ，整数的 数组形式 ，和整数 k ，返回 整数 num + k 的 数组形式 。
+ *
+ * 示例 1：
+ * 输入：num = [1,2,0,0], k = 34
+ * 输出：[1,2,3,4]
+ * 解释：1200 + 34 = 1234
+ *
+ * 示例 2：
+ * 输入：num = [2,7,4], k = 181
+ * 输出：[4,5,5]
+ * 解释：274 + 181 = 455
+ *
+ * 示例 3：
+ * 输入：num = [2,1,5], k = 806
+ * 输出：[1,0,2,1]
+ * 解释：215 + 806 = 1021
+ *
+ * 提示：
+ * 1 <= num.length <= 104
+ * 0 <= num[i] <= 9
+ * num 不包含任何前导零，除了零本身
+ * 1 <= k <= 104
+ * */
+const addToArrayForm = (num, k) => {
+    let count = BigInt(num.join(''))
+    let res = (count + BigInt(k)).toString()
+    let arr = []
+    for (let i = 0; i < res.length; i++) {
+        arr.push(Number(res[i]))
+    }
+    return arr
+}
+// console.log(addToArrayForm([1,2,6,3,0,7,1,7,1,9,7,5,6,6,4,4,0,0,6,3], 516))
+
+/**
  * 997. 找到小镇的法官
  *
  * 小镇里有 n 个人，按从 1 到 n 的顺序编号。传言称，这些人中有一个暗地里是小镇法官。
