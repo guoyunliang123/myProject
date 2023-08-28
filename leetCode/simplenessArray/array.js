ab510da5b10510c1bc4204e9ff4067d99a3390cb
郭云亮 <1060950677@qq.com>
2023-08-28 10:45:41 +0800

    feat：leetcode：1037 - 有效的回旋镖

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -2149,7 +2149,34 @@ const prefixesDivBy5 = (nums) => {
     }
     return answer;
 }
-console.log(prefixesDivBy5([0,1,1,1,1,1]))
+// console.log(prefixesDivBy5([0,1,1,1,1,1]))
+
+/**
+ * 1037. 有效的回旋镖
+ *
+ * 给定一个数组 points ，其中 points[i] = [xi, yi] 表示 X-Y 平面上的一个点，如果这些点构成一个 回旋镖 则返回 true 。
+ * 回旋镖 定义为一组三个点，这些点 各不相同 且 不在一条直线上 。
+ *
+ * 示例 1：
+ * 输入：points = [[1,1],[2,3],[3,2]]
+ * 输出：true
+ *
+ * 示例 2：
+ * 输入：points = [[1,1],[2,2],[3,3]]
+ * 输出：false
+ *
+ * 提示：
+ * points.length == 3
+ * points[i].length == 2
+ * 0 <= xi, yi <= 100
+ * */
+const isBoomerang = (points) => {
+    return (points[1][[1]] - points[0][1]) * (points[2][[0]] - points[1][0]) !== (points[2][[1]] - points[1][1]) * (points[1][[0]] - points[0][0])
+
+    // 另一个方法：判断三个点是否能构成三角形，如果可以则为有效的回旋镖
+    // 三个点能否构成三角形公式：(Ax * (By - Cy) + Bx * (Cy - Ay) + Cx * (Ay - By)) / 2 !== 0
+}
+// console.log(isBoomerang([[1,1],[1,2],[1,3]]))
 
 /**
  * 1046. 最后一块石头的重量
