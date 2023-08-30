84c09f4fc37619e3399be15a8733d7fa7903e984
郭云亮 <1060950677@qq.com>
2023-08-30 11:07:43 +0800

    feat：leetcode：896 - 单调数列

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -1808,6 +1808,78 @@ const fairCandySwap = (aliceSizes, bobSizes) => {
 // console.log(fairCandySwap([1, 2, 5], [2, 4]))
 
 /**
+ * 896. 单调数列
+ *
+ * 如果数组是单调递增或单调递减的，那么它是 单调 的。
+ *
+ * 如果对于所有 i <= j，nums[i] <= nums[j]，那么数组 nums 是单调递增的。 如果对于所有 i <= j，nums[i]> = nums[j]，那么数组 nums 是单调递减的。
+ *
+ * 当给定的数组 nums 是单调数组时返回 true，否则返回 false。
+ *
+ * 示例 1：
+ * 输入：nums = [1,2,2,3]
+ * 输出：true
+ *
+ * 示例 2：
+ * 输入：nums = [6,5,4,4]
+ * 输出：true
+ *
+ * 示例 3：
+ * 输入：nums = [1,3,2]
+ * 输出：false
+ *
+ * 提示：
+ * 1 <= nums.length <= 105
+ * -105 <= nums[i] <= 105
+ * */
+const isMonotonic = (nums) => {
+    // 方法一：丑陋
+    // let len = nums.length;
+    // if (len < 3) return true;
+    // if (new Set(nums).size === 1) return true;
+    // let flag = ''; // increase 增   decrease 减
+    // for (let i = 1; i < len; i++) {
+    //     console.log(nums[i] - nums[i - 1], 's')
+    //     if (nums[i] - nums[i - 1] < 0) {
+    //         flag = 'decrease'
+    //         break;
+    //     } else if (nums[i] - nums[i - 1] > 0) {
+    //         flag = 'increase'
+    //         break;
+    //     }
+    // }
+    // for (let i = 1; i < len; i++) {
+    //     if (flag === 'increase') {
+    //         if (nums[i] - nums[i - 1] < 0) return false;
+    //     } else if (flag === 'decrease') {
+    //         if (nums[i] - nums[i - 1] > 0) return false;
+    //     }
+    // }
+    // return true;
+
+    // 方法2：
+    const len = nums.length;
+    let state = 0;
+    for (let i = 1; i < len; i++) {
+        if (nums[i - 1] > nums[i]) {
+            if (state === 0) {
+                state = -1
+            } else if (state === 1) {
+                return false;
+            }
+        } else if (nums[i - 1] < nums[i]) {
+            if (state === 0) {
+                state = 1
+            } else if (state === -1) {
+                return false
+            }
+        }
+    }
+    return true;
+}
+// console.log(isMonotonic([1, 2 , 2, 3]))
+
+/**
  * 905. 按奇偶排序数组
  *
  * 给你一个整数数组 nums，将 nums 中的的所有偶数元素移动到数组的前面，后跟所有奇数元素。
