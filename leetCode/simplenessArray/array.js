dcd5dce9464751ac7bee37f3ffdc291f42c6903b
郭云亮 <1060950677@qq.com>
2023-08-30 16:02:17 +0800

    feat：leetcode：766 - 托普利茨矩阵

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -1487,6 +1487,47 @@ const dominantIndex = (nums) => {
 // console.log(dominantIndex([3, 6, 1, 0]))
 
 /**
+ * 766. 托普利茨矩阵
+ *
+ * 给你一个 m x n 的矩阵 matrix 。如果这个矩阵是托普利茨矩阵，返回 true ；否则，返回 false 。
+ *
+ * 如果矩阵上每一条由左上到右下的对角线上的元素都相同，那么这个矩阵是 托普利茨矩阵 。
+ *
+ * 示例 1：
+ * 输入：matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+ * 输出：true
+ * 解释：
+ * 在上述矩阵中, 其对角线为:
+ * "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]"。
+ * 各条对角线上的所有元素均相同, 因此答案是 True 。
+ *
+ * 示例 2：
+ * 输入：matrix = [[1,2],[2,2]]
+ * 输出：false
+ * 解释：
+ * 对角线 "[1, 2]" 上的元素不同。
+ *
+ * 提示：
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 20
+ * 0 <= matrix[i][j] <= 99
+ * */
+const isToeplitzMatrix = (matrix) => {
+    let rows = matrix.length, cols = matrix[0].length;
+    for (let col = 1; col < cols; col++) {
+        for (let row = 1; row < rows; row++) {
+            if (matrix[row - 1][col - 1] !== matrix[row][col]) {
+                return false
+            }
+        }
+    }
+    return true;
+}
+// console.log(isToeplitzMatrix([[1,2,3,4],[5,1,2,3],[9,5,1,2]]))
+// console.log(isToeplitzMatrix([[1,2],[2,2]]))
+
+/**
  * 804. 唯一摩尔斯密码词
  *
  * 国际摩尔斯密码定义一种标准编码方式，将每个字母对应于一个由一系列点和短线组成的字符串， 比如:
