4c62c1ecab805d7d1a60c1cb000f57d894953340
郭云亮 <1060950677@qq.com>
2023-08-31 17:45:22 +0800

    feat：leetcode：1030 - 距离顺序排列矩阵单元格

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -2547,6 +2547,72 @@ const prefixesDivBy5 = (nums) => {
 // console.log(prefixesDivBy5([0,1,1,1,1,1]))
 
 /**
+ * 1030. 距离顺序排列矩阵单元格
+ *
+ * 给定四个整数 rows ,   cols ,  rCenter 和 cCenter 。有一个 rows x cols 的矩阵，你在单元格上的坐标是 (rCenter, cCenter) 。
+ *
+ * 返回矩阵中的所有单元格的坐标，并按与 (rCenter, cCenter) 的 距离 从最小到最大的顺序排。你可以按 任何 满足此条件的顺序返回答案。
+ *
+ * 单元格(r1, c1) 和 (r2, c2) 之间的距离为|r1 - r2| + |c1 - c2|。
+ *
+ * 示例 1：
+ * 输入：rows = 1, cols = 2, rCenter = 0, cCenter = 0
+ * 输出：[[0,0],[0,1]]
+ * 解释：从 (r0, c0) 到其他单元格的距离为：[0,1]
+ *
+ * 示例 2：
+ * 输入：rows = 2, cols = 2, rCenter = 0, cCenter = 1
+ * 输出：[[0,1],[0,0],[1,1],[1,0]]
+ * 解释：从 (r0, c0) 到其他单元格的距离为：[0,1,1,2]
+ * [[0,1],[1,1],[0,0],[1,0]] 也会被视作正确答案。
+ *
+ * 示例 3：
+ * 输入：rows = 2, cols = 3, rCenter = 1, cCenter = 2
+ * 输出：[[1,2],[0,2],[1,1],[0,1],[1,0],[0,0]]
+ * 解释：从 (r0, c0) 到其他单元格的距离为：[0,1,1,2,2,3]
+ * 其他满足题目要求的答案也会被视为正确，例如 [[1,2],[1,1],[0,2],[1,0],[0,1],[0,0]]。
+ *
+ * 提示：
+ * 1 <= rows, cols <= 100
+ * 0 <= rCenter < rows
+ * 0 <= cCenter < cols
+ * */
+const allCellsDistOrder = (rows, cols, rCenter, cCenter) => {
+    // 方法1：
+    // let distanceArr = [], result = [];
+    // for (let row = 0; row < rows; row++) {
+    //     for (let col = 0; col < cols; col++) {
+    //         distanceArr.push(
+    //             {
+    //                 len: Math.abs(row - rCenter) + Math.abs(col - cCenter),
+    //                 point: [row, col]
+    //             }
+    //         )
+    //     }
+    // }
+    // distanceArr.sort((a, b) => a.len - b.len);
+    // for (let i = 0; i < distanceArr.length; i++) {
+    //     result.push(distanceArr[i].point);
+    // }
+    // return result;
+
+    // 方法2：
+    let res = [];
+    for (let row = 0; row < rows; row++) {
+        for (let col = 0; col < cols; col++) {
+            res.push([row, col])
+        }
+    }
+    res.sort((a, b) => {
+        const x1 = Math.abs(a[0] - rCenter) + Math.abs(a[1] - cCenter)
+        const x2 = Math.abs(b[0] - rCenter) + Math.abs(b[1] - cCenter)
+        return x1- x2
+    })
+    return res;
+}
+// console.log(allCellsDistOrder(2, 3, 0, 1))
+
+/**
  * 1037. 有效的回旋镖
  *
  * 给定一个数组 points ，其中 points[i] = [xi, yi] 表示 X-Y 平面上的一个点，如果这些点构成一个 回旋镖 则返回 true 。
