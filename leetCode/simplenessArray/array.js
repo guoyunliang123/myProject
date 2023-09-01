c765f66bd49033630317f48effef811fddc6baeb
郭云亮 <1060950677@qq.com>
2023-09-01 10:20:20 +0800

    feat：leetcode：1013 - 将数组分成和相等的三个部分

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -2511,6 +2511,72 @@ const commonChars = (words) => {
 // console.log(commonChars(["bella","label","roller"]))
 
 /**
+ * 1013. 将数组分成和相等的三个部分
+ *
+ * 给你一个整数数组 arr，只有可以将其划分为三个和相等的 非空 部分时才返回 true，否则返回 false。
+ *
+ * 形式上，如果可以找出索引 i + 1 < j 且满足 (arr[0] + arr[1] + ... + arr[i] == arr[i + 1] + arr[i + 2] + ... + arr[j - 1] == arr[j] + arr[j + 1] + ... + arr[arr.length - 1]) 就可以将数组三等分。
+ *
+ * 示例 1：
+ * 输入：arr = [0,2,1,-6,6,-7,9,1,2,0,1]
+ * 输出：true
+ * 解释：0 + 2 + 1 = -6 + 6 - 7 + 9 + 1 = 2 + 0 + 1
+ *
+ * 示例 2：
+ * 输入：arr = [0,2,1,-6,6,7,9,-1,2,0,1]
+ * 输出：false
+ *
+ * 示例 3：
+ * 输入：arr = [3,3,6,5,-2,2,5,1,-9,4]
+ * 输出：true
+ * 解释：3 + 3 = 6 = 5 - 2 + 2 + 5 + 1 - 9 + 4
+ *
+ * 提示：
+ * 3 <= arr.length <= 5 * 104
+ * -104 <= arr[i] <= 104
+ * */
+const canThreePartsEqualSum = (arr) => {
+    // 方法1：
+    // let count = arr.reduce((a, b) => a + b, 0)
+    // if (count % 3 !== 0) return false;
+    // let sum = 0, part = count / 3;
+    // let res = 0
+    // for (let i = 0; i < arr.length; i++) {
+    //     sum += arr[i];
+    //     if (sum === part) {
+    //         sum = 0;
+    //         res++
+    //     }
+    // }
+    // return res >= 3
+
+    // 方法2：
+    let sum = 0
+    for (const val of arr) {
+        sum += val;
+    }
+    if (sum % 3 !== 0) return false;
+    let left = 0, right = arr.length - 1;
+    let leftSum = arr[left], rightSum = arr[right];
+    const avNum = sum / 3;
+    while (left + 1 < right) {
+        if (leftSum === avNum && rightSum === avNum) {
+            return true
+        }
+        if (leftSum !== avNum) {
+            left++
+            leftSum += arr[left]
+        }
+        if (rightSum !== avNum) {
+            right++
+            rightSum += arr[right]
+        }
+    }
+    return false;
+}
+console.log(canThreePartsEqualSum([0,2,1,-6,6,-7,9,1,2,0,1]))
+
+/**
  * 1018. 可被 5 整除的二进制前缀
  *
  * 给定一个二进制数组 nums ( 索引从0开始 )。
