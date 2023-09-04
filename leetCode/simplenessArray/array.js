2c1dbfcf612bba646678358e165985574fa680e3
郭云亮 <1060950677@qq.com>
2023-09-04 09:45:25 +0800

    feat：leetcode：k 次取反后最大化的数组值

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -2511,6 +2511,67 @@ const commonChars = (words) => {
 // console.log(commonChars(["bella","label","roller"]))
 
 /**
+ * 1005. K 次取反后最大化的数组和
+ *
+ * 给你一个整数数组 nums 和一个整数 k ，按以下方法修改该数组：
+ *
+ * 选择某个下标 i 并将 nums[i] 替换为 -nums[i] 。
+ * 重复这个过程恰好 k 次。可以多次选择同一个下标 i 。
+ *
+ * 以这种方式修改数组后，返回数组 可能的最大和 。
+ *
+ * 示例 1：
+ * 输入：nums = [4,2,3], k = 1
+ * 输出：5
+ * 解释：选择下标 1 ，nums 变为 [4,-2,3] 。
+ *
+ * 示例 2：
+ * 输入：nums = [3,-1,0,2], k = 3
+ * 输出：6
+ * 解释：选择下标 (1, 2, 2) ，nums 变为 [3,1,0,2] 。
+ *
+ * 示例 3：
+ * 输入：nums = [2,-3,-1,5,-4], k = 2
+ * 输出：13
+ * 解释：选择下标 (1, 4) ，nums 变为 [2,3,-1,5,4] 。
+ *
+ * 提示：
+ * 1 <= nums.length <= 104
+ * -100 <= nums[i] <= 100
+ * 1 <= k <= 104
+ * */
+const largestSumAfterKNegations = (nums, k) => {
+    const len = nums.length;
+    if (k > len) {
+        nums.sort((a, b) => Math.abs(a) - Math.abs(b))
+        let x = k - nums.filter(item => item < 0).length;
+        if (x % 2 === 0) {
+            return nums.reduce((a, b) => Math.abs(a) + Math.abs(b), 0)
+        } else {
+            return nums.reduce((a, b) => Math.abs(a) + Math.abs(b)) - (Math.abs(nums[0]) * 2)
+        }
+    } else {
+        nums.sort((a, b) => a - b);
+        for (let i = 0; i < len; i++) {
+            if (nums[i] < 0 && k > 0) {
+                nums[i] = -nums[i];
+                k--;
+            } else if (nums[i] === 0) {
+                k = 0;
+            } else if (nums[i] > 0 && k > 0) {
+                if (k % 2 === 1) {
+                    nums.sort((a, b) => a - b);
+                    nums[0] = -nums[0];
+                }
+                k = 0;
+            }
+        }
+        return nums.reduce((a, b) => a + b)
+    }
+}
+// console.log(largestSumAfterKNegations([-4,-2,-3], 4));
+
+/**
  * 1013. 将数组分成和相等的三个部分
  *
  * 给你一个整数数组 arr，只有可以将其划分为三个和相等的 非空 部分时才返回 true，否则返回 false。
@@ -2574,7 +2635,7 @@ const canThreePartsEqualSum = (arr) => {
     }
     return false;
 }
-console.log(canThreePartsEqualSum([0,2,1,-6,6,-7,9,1,2,0,1]))
+// console.log(canThreePartsEqualSum([0,2,1,-6,6,-7,9,1,2,0,1]))
 
 /**
  * 1018. 可被 5 整除的二进制前缀
