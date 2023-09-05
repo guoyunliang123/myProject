04aed6fa9405431d70df2b8dd82a9db3becaa450
郭云亮 <1060950677@qq.com>
2023-09-05 10:53:11 +0800

    feat：leetcode：914 - 卡牌分组

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -2074,6 +2074,61 @@ const smallestRangeI = (nums, k) => {
 // console.log(smallestRangeI([1, 3, 6], 3))
 
 /**
+ * 914. 卡牌分组
+ *
+ * 给定一副牌，每张牌上都写着一个整数。
+ *
+ * 此时，你需要选定一个数字 X，使我们可以将整副牌按下述规则分成 1 组或更多组：
+ *  每组都有 X 张牌。
+ *  组内所有的牌上都写着相同的整数。
+ *
+ * 仅当你可选的 X >= 2 时返回 true。
+ *
+ * 示例 1：
+ * 输入：deck = [1,2,3,4,4,3,2,1]
+ * 输出：true
+ * 解释：可行的分组是 [1,1]，[2,2]，[3,3]，[4,4]
+ *
+ * 示例 2：
+ * 输入：deck = [1,1,1,2,2,2,3,3]
+ * 输出：false
+ * 解释：没有满足要求的分组。
+ *
+ * 提示：
+ * 1 <= deck.length <= 104
+ * 0 <= deck[i] < 104
+ * */
+const hasGroupsSizeX = (deck) => {
+    let map = new Map();
+    for (let i = 0; i < deck.length; i++) {
+        if (!map.has(deck[i])) {
+            map.set(deck[i], 1);
+        } else {
+            map.set(deck[i], map.get(deck[i]) + 1)
+        }
+    }
+    const vals = Array.from(new Set(map.values()));
+    let min = vals[0];
+    for (let i = 1; i < vals.length; i++) {
+        min = gcd(min, vals[i])
+    }
+    // 获取最大公约数
+    function gcd(a, b){
+        return b !== 0 ? gcd(b, a % b) : a;
+    }
+    return min >= 2;
+    // const res = new Set(vals);
+    // // 解构获取 set 对象的第一个元素
+    // const [first] = res;
+    // console.log(first, 'first')
+    // return res.size === 1 && first >= 2;
+}
+// console.log(hasGroupsSizeX([1,2,3,4,4,3,2,1]))
+// console.log(hasGroupsSizeX([1,1,1,2,2,2,3,3]))
+// console.log(hasGroupsSizeX([1,1,1,2,2,2,3,3,3]))
+// console.log(hasGroupsSizeX([1,1,1,1,2,2,2,2,2,2]))
+
+/**
  * 922. 按奇偶排序数组 II
  *
  * 给定一个非负整数数组 nums，  nums 中一半整数是 奇数 ，一半整数是 偶数 。
