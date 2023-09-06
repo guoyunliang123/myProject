3db088e97af1395237b5fedf0b3cf5b581cacab1
郭云亮 <1060950677@qq.com>
2023-09-06 16:56:19 +0800

    feat：leetcode：1089 - 复写零

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -1501,8 +1501,8 @@ const nextGreatestLetter = (letters, target) => {
     if (letters[0] >= target || letters.at(-1) < target) return letters[0];
     return letters.find(item => item > target)
 };
-console.log(nextGreatestLetter(["c","f","j"], "a"))
-console.log(nextGreatestLetter(["x","x","y","y"], "z"))
+// console.log(nextGreatestLetter(["c","f","j"], "a"))
+// console.log(nextGreatestLetter(["x","x","y","y"], "z"))
 
 /**
  * 746. 使用最小花费爬楼梯
@@ -3080,3 +3080,51 @@ const heightChecker = (heights) => {
     return count;
 }
 // console.log(heightChecker([5,1,2,3,4]))
+
+/**
+ * 1089. 复写零
+ *
+ * 给你一个长度固定的整数数组 arr ，请你将该数组中出现的每个零都复写一遍，并将其余的元素向右平移。
+ *
+ * 注意：请不要在超过该数组长度的位置写入元素。请对输入的数组 就地 进行上述修改，不要从函数返回任何东西。
+ *
+ * 示例 1：
+ * 输入：arr = [1,0,2,3,0,4,5,0]
+ * 输出：[1,0,0,2,3,0,0,4]
+ * 解释：调用函数后，输入的数组将被修改为：[1,0,0,2,3,0,0,4]
+ *
+ * 示例 2：
+ * 输入：arr = [1,2,3]
+ * 输出：[1,2,3]
+ * 解释：调用函数后，输入的数组将被修改为：[1,2,3]
+ *
+ * 提示：
+ * 1 <= arr.length <= 104
+ * 0 <= arr[i] <= 9
+ * */
+const duplicateZeros = (arr) => {
+    // 方法1：
+    // function overWrite (arr, index) {
+    //     for (let i = arr.length - 1; i > index; i--) {
+    //         arr[i] = arr[i - 1]
+    //     }
+    // }
+    // for (let i = arr.length - 1; i >= 0; i--) {
+    //     if (arr[i] === 0) {
+    //         overWrite(arr, i)
+    //     }
+    // }
+
+    // 方法2：
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        if (arr[i] === 0) {
+            arr.splice(i, 0, 0);
+            i++;
+        }
+    }
+    while (arr.length > len) {
+        arr.pop();
+    }
+}
+console.log(duplicateZeros([1,0,2,3,0,4,5,0]))
