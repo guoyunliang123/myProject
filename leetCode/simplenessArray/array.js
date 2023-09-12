07c33756090b663bd0e42736c1b1dcd87812d9f3
郭云亮 <1060950677@qq.com>
2023-09-12 14:57:34 +0800

    feat：leetcode：1160 - 拼写单词

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -3264,3 +3264,85 @@ const numEquivDominoPairs = (dominoes) => {
     return res;
 }
 // console.log(numEquivDominoPairs([[1,2],[1,2],[1,1],[1,2],[1,1],[2,1]]))
+
+/**
+ * 1160. 拼写单词
+ *
+ * 给你一份『词汇表』（字符串数组） words 和一张『字母表』（字符串） chars。
+ *
+ * 假如你可以用 chars 中的『字母』（字符）拼写出 words 中的某个『单词』（字符串），那么我们就认为你掌握了这个单词。
+ *
+ * 注意：每次拼写（指拼写词汇表中的一个单词）时，chars 中的每个字母都只能用一次。
+ *
+ * 返回词汇表 words 中你掌握的所有单词的 长度之和。
+ *
+ * 示例 1：
+ * 输入：words = ["cat","bt","hat","tree"], chars = "atach"
+ * 输出：6
+ * 解释：
+ * 可以形成字符串 "cat" 和 "hat"，所以答案是 3 + 3 = 6。
+ *
+ * 示例 2：
+ * 输入：words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+ * 输出：10
+ * 解释：
+ * 可以形成字符串 "hello" 和 "world"，所以答案是 5 + 5 = 10。
+ *
+ * 提示：
+ * 1 <= words.length <= 1000
+ * 1 <= words[i].length, chars.length <= 100
+ * 所有字符串中都仅包含小写英文字母
+ * */
+const countCharacters = (words, chars) => {
+    // 方法1：
+    // const hash = {};
+    // for (let i = 0; i < chars.length; i++) {
+    //     if (!hash[chars[i]]) {
+    //         hash[chars[i]] = 1;
+    //     } else {
+    //         hash[chars[i]] += 1;
+    //     }
+    // }
+    // let count = 0;
+    // for (let i = 0; i < words.length; i++) {
+    //     let word = words[i];
+    //     console.log(wordLine(word), 's')
+    //     count += wordLine(word);
+    // }
+    // return count;
+    // function wordLine(str) {
+    //     let newHash = Object.assign({}, hash);
+    //     let count = 0;
+    //     for (let i = 0; i < str.length; i++) {
+    //         if (!newHash[str[i]] || newHash[str[i]] === 0) return 0;
+    //         newHash[str[i]] -= 1;
+    //         count +=1;
+    //     }
+    //     return count;
+    // }
+
+    // 方法2：
+    const state = Array(26).fill(0);
+    for (let i = 0; i < chars.length; i++) {
+        state[chars[i].charCodeAt() - 'a'.charCodeAt()]++;
+    }
+    console.log(state, 'state')
+    let sum = 0;
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        const stateCopy = Array(26).fill(0);
+        for (let j = 0; j < word.length; j++) {
+            const ch = word[j].charCodeAt() - 'a'.charCodeAt();
+            stateCopy[ch]++;
+            if(stateCopy[ch] > state[ch]) {
+                break;
+            }
+        }
+        if(j === word.length) {
+            sum += j;
+        }
+    }
+    return sum;
+}
+// console.log(countCharacters(["hello","world","leetcode"], "welldonehoneyr"))
+// console.log(countCharacters(["hello"], "helo"))
