4111339c170b7b314e52c1b3eb4b8d1970a62730
郭云亮 <1060950677@qq.com>
2023-09-14 17:25:19 +0800

    feat：leetcode：1260 - 二维网格迁移

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -3611,3 +3611,66 @@ const oddCells = (m, n, indices) => {
 }
 // console.log(oddCells(2, 3, [[0, 1], [1, 1]]))
 // console.log(oddCells(48, 37, [[40,5]]))
+
+/**
+ * 1260. 二维网格迁移
+ *
+ * 给你一个 m 行 n 列的二维网格 grid 和一个整数 k。你需要将 grid 迁移 k 次。
+ *
+ * 每次「迁移」操作将会引发下述活动：
+ *
+ * 位于 grid[i][j] 的元素将会移动到 grid[i][j + 1]。
+ * 位于 grid[i][n - 1] 的元素将会移动到 grid[i + 1][0]。
+ * 位于 grid[m - 1][n - 1] 的元素将会移动到 grid[0][0]。
+ * 请你返回 k 次迁移操作后最终得到的 二维网格。
+ *
+ * 示例 1：
+ * 输入：grid = [[1,2,3],[4,5,6],[7,8,9]], k = 1
+ * 输出：[[9,1,2],[3,4,5],[6,7,8]]
+ *
+ * 示例 2：
+ * 输入：grid = [[3,8,1,9],[19,7,2,5],[4,6,11,10],[12,0,21,13]], k = 4
+ * 输出：[[12,0,21,13],[3,8,1,9],[19,7,2,5],[4,6,11,10]]
+ *
+ * 示例 3：
+ * 输入：grid = [[1,2,3],[4,5,6],[7,8,9]], k = 9
+ * 输出：[[1,2,3],[4,5,6],[7,8,9]]
+ *
+ * 提示：
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m <= 50
+ * 1 <= n <= 50
+ * -1000 <= grid[i][j] <= 1000
+ * 0 <= k <= 100
+ * */
+const shiftGrid = (grid, k) => {
+    // 方法1：
+    // let len = grid.length;
+    // let len1 = grid[0].length;
+    // let arr = grid.flat();
+    // let k1 = k % arr.length; // 考虑当 k > arr.length 的情况。
+    // let tag = arr.length - k1;
+    // let left = arr.slice(0, tag);
+    // let right = arr.slice(tag, arr.length)
+    // let newArr = [...right, ...left];
+    // let result = [];
+    // for (let i = 0; i < len; i++) {
+    //     let temp = newArr.slice(i * len1, i * len1 + len1)
+    //     result.push(temp);
+    // }
+    // return result;
+
+    // 方法2：
+    const n = grid.length, m = grid[0].length, tot = n * m;
+    const ans = Array.from(Array(n), _ => Array(m));
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
+            let t = (i * m + j + k) % tot;
+            ans[t / m | 0][t % m] = grid[i][j];
+        }
+    }
+    return ans;
+}
+// console.log(shiftGrid([[3,8,1,9],[19,7,2,5],[4,6,11,10],[12,0,21,13]], k = 4))
+console.log(shiftGrid([[1,2,3],[4,5,6],[7,8,9]], k = 1))
