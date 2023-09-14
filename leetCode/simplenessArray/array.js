e1bfd7ab696f3fde6d36ed06982427c09a25e1bb
郭云亮 <1060950677@qq.com>
2023-09-14 09:46:07 +0800

    feat：leetcode：1232 - 缀点成线

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -3513,3 +3513,51 @@ const uniqueOccurrences = (arr) => {
     return true
 }
 // console.log(uniqueOccurrences([1, 2, 2, 1, 1, 2]))
+
+/**
+ * 1232. 缀点成线
+ *
+ * 给定一个数组 coordinates ，其中 coordinates[i] = [x, y] ， [x, y] 表示横坐标为 x、纵坐标为 y 的点。请你来判断，这些点是否在该坐标系中属于同一条直线上。
+ *
+ * 示例 1：
+ * 输入：coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+ * 输出：true
+ *
+ * 示例 2：
+ * 输入：coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+ * 输出：false
+ *
+ * 提示：
+ * 2 <= coordinates.length <= 1000
+ * coordinates[i].length == 2
+ * -10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
+ * coordinates 中不含重复的点
+ * */
+const checkStraightLine = (coordinates) => {
+    // tips: 两点斜率计算
+    // A(a1 ,a2)   B(b1,b2 )   C(c1,c2 )
+    // 直线AB的斜率  k1=(b2-a2) \ ( a2-a1)
+    // 直线BC的斜率  k2=(c2-b2) \  (c1-b1)
+    let slope = coordinates[1][0] - coordinates[0][0] === 0 ? 'Infinity' : (coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] - coordinates[0][0])
+    for (let i = 2; i < coordinates.length; i++) {
+        let col = coordinates[i][0] - coordinates[0][0];
+        let row = coordinates[i][1] - coordinates[0][1];
+        if (col === 0) {
+            if (slope !== 'Infinity') {
+                return false;
+            }
+        } else if (row === 0) {
+            if (slope === 'Infinity') {
+                return false;
+            }
+        } else {
+            if (slope !== (row / col)) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
+// console.log(checkStraightLine([[1,2],[2,3],[3,4],[4,5],[5,6]]));
+// console.log(checkStraightLine([[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]));
+// console.log(checkStraightLine([[0,0],[0,1],[0,-1]]));
