d56b359104537f71827ff53dd55e82d423c421a1
郭云亮 <1060950677@qq.com>
2023-09-15 14:18:27 +0800

    feat：leetcode：1287 - 有序数组中出现次数超过 25% 的元素

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -3673,4 +3673,43 @@ const shiftGrid = (grid, k) => {
     return ans;
 }
 // console.log(shiftGrid([[3,8,1,9],[19,7,2,5],[4,6,11,10],[12,0,21,13]], k = 4))
-console.log(shiftGrid([[1,2,3],[4,5,6],[7,8,9]], k = 1))
+// console.log(shiftGrid([[1,2,3],[4,5,6],[7,8,9]], k = 1))
+
+/**
+ * 1287. 有序数组中出现次数超过25%的元素
+ *
+ * 给你一个非递减的 有序 整数数组，已知这个数组中恰好有一个整数，它的出现次数超过数组元素总数的 25%。
+ *
+ * 请你找到并返回这个整数
+ *
+ * 示例：
+ * 输入：arr = [1,2,2,6,6,6,6,7,10]
+ * 输出：6
+ *
+ * 提示：
+ * 1 <= arr.length <= 10^4
+ * 0 <= arr[i] <= 10^5
+ * */
+const findSpecialInteger = (arr) => {
+    // 方法1：
+    // let len = arr.length;
+    // if (len === 1) return arr[0];
+    // let map = new Map();
+    // let tag = len / 4;
+    // for (let i = 0; i < len; i++) {
+    //     if (!map.has(arr[i])) {
+    //         map.set(arr[i], 1)
+    //     } else {
+    //         if (map.get(arr[i]) + 1 > tag) return arr[i]
+    //         map.set(arr[i], map.get(arr[i]) + 1)
+    //     }
+    // }
+
+    // 方法2：
+    if (arr.length <= 3) return arr[0];
+    const len = arr.length % 4 === 0 ? arr.length / 4 : ((arr.length / 4) >> 0) + 1;
+    for (let i = len; i < arr.length; i++) {
+        if (arr[i - len] === arr[i]) return arr[i]
+    }
+}
+console.log(findSpecialInteger([1,2,2,6,6,6,6,7,10]));
