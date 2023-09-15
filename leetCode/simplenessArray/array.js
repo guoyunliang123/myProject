9dd4c473f141169731269f10a6ca4504338ba901
郭云亮 <1060950677@qq.com>
2023-09-15 17:39:54 +0800

    feat：leetcode：1299 - 将两个元素替换为右侧最大元素

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -3750,3 +3750,53 @@ const findNumbers = (nums) => {
     return count;
 }
 // console.log(findNumbers([12,345,2,6,7896]));
+
+/**
+ * 1299. 将每个元素替换为右侧最大元素
+ *
+ * 给你一个数组 arr ，请你将每个元素用它右边最大的元素替换，如果是最后一个元素，用 -1 替换。
+ *
+ * 完成所有替换操作后，请你返回这个数组。
+ *
+ * 示例 1：
+ * 输入：arr = [17,18,5,4,6,1]
+ * 输出：[18,6,6,6,1,-1]
+ * 解释：
+ * - 下标 0 的元素 --> 右侧最大元素是下标 1 的元素 (18)
+ * - 下标 1 的元素 --> 右侧最大元素是下标 4 的元素 (6)
+ * - 下标 2 的元素 --> 右侧最大元素是下标 4 的元素 (6)
+ * - 下标 3 的元素 --> 右侧最大元素是下标 4 的元素 (6)
+ * - 下标 4 的元素 --> 右侧最大元素是下标 5 的元素 (1)
+ * - 下标 5 的元素 --> 右侧没有其他元素，替换为 -1
+ *
+ * 示例 2：
+ * 输入：arr = [400]
+ * 输出：[-1]
+ * 解释：下标 0 的元素右侧没有其他元素。
+ *
+ * 提示：
+ * 1 <= arr.length <= 104
+ * 1 <= arr[i] <= 105
+ * */
+const replaceElements = (arr) => {
+    // 方法1：4500+ms
+    // let len = arr.length, res = [];
+    // function maxNum(arr, index) {
+    //     if (index === len - 1) return -1;
+    //     return Math.max(...arr.slice(index + 1))
+    // }
+    // for (let i = 0; i < len; i++) {
+    //     res.push(maxNum(arr, i))
+    // }
+    // return res;
+
+    // 方法2：
+    // let len = arr.length, res = new Array(len).fill(0);
+    // let max = -1;
+    // for (let i = len - 1; i >= 0; i--) {
+    //     res[i] = max;
+    //     max = Math.max(max, arr[i])
+    // }
+    // return res;
+};
+// console.log(replaceElements([17,18,5,4,6,1]));
