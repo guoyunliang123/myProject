6bc608b601056b9923478abc4ca2d65fd06e426a
郭云亮 <1060950677@qq.com>
2023-09-19 15:04:00 +0800

    feat：leetcode：1346 - 检查整数及其两倍数是否存在

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -3999,3 +3999,62 @@ const kWeakestRows = (mat, k) => {
 }
 // console.log(kWeakestRows([[1,1,0,0,0],[1,1,1,1,0],[1,0,0,0,0],[1,1,0,0,0],[1,1,1,1,1]], 3));
 // console.log(kWeakestRows([[1,0],[1,0],[1,0],[1,1]], 4));
+
+/**
+ * 1346. 检查整数及其两倍数是否存在
+ *
+ * 给你一个整数数组 arr，请你检查是否存在两个整数 N 和 M，满足 N 是 M 的两倍（即，N = 2 * M）。
+ *
+ * 更正式地，检查是否存在两个下标 i 和 j 满足：
+ *
+ * i != j
+ * 0 <= i, j < arr.length
+ * arr[i] == 2 * arr[j]
+ *
+ * 示例 1：
+ * 输入：arr = [10,2,5,3]
+ * 输出：true
+ * 解释：N = 10 是 M = 5 的两倍，即 10 = 2 * 5 。
+ *
+ * 示例 2：
+ * 输入：arr = [7,1,14,11]
+ * 输出：true
+ * 解释：N = 14 是 M = 7 的两倍，即 14 = 2 * 7 。
+ *
+ * 示例 3：
+ * 输入：arr = [3,1,7,11]
+ * 输出：false
+ * 解释：在该情况下不存在 N 和 M 满足 N = 2 * M 。
+ *
+ * 提示：
+ * 2 <= arr.length <= 500
+ * -10^3 <= arr[i] <= 10^3
+ * */
+const checkIfExist = (arr) => {
+    // 方法1：
+    // let doubleArr = [];
+    // arr.map(item => {
+    //     if (item !== 0) {
+    //         doubleArr.push(item * 2)
+    //     }
+    // });
+    // if (arr.length - doubleArr.length >= 2) return true; // 数组中 0 的个数 >= 2 返回 true
+    // return arr.some(item => doubleArr.includes(item))
+
+    // 方法2：
+    let hash = {};
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        let num = arr[i];
+        if (num === 0 && hash[num]) {
+            return true
+        } else {
+            if (hash[2 * num] || (num % 2 === 0 && hash[num / 2])) {
+                return true
+            }
+            hash[num] = true
+        }
+    }
+    return false;
+};
+// console.log(checkIfExist([-2,0,10,-19,4,6,-8]))
