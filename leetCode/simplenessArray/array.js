5444585a1e19c7c6047a09a239f7c856da2ff12e
郭云亮 <1060950677@qq.com>
2023-09-19 11:19:18 +0800

    feat：leetcode：1337 - 矩阵中战斗力最弱的 k 行

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -3928,3 +3928,74 @@ const arrayRankTransform = (arr) => {
 }
 // console.log(arrayRankTransform([40, 10, 20, 30]))
 // console.log(arrayRankTransform([37,12,28,9,100,56,80,5,12]))
+
+/**
+ * 1337. 矩阵中战斗力最弱的 K 行
+ *
+ * 给你一个大小为 m * n 的矩阵 mat，矩阵由若干军人和平民组成，分别用 1 和 0 表示。
+ *
+ * 请你返回矩阵中战斗力最弱的 k 行的索引，按从最弱到最强排序。
+ *
+ * 如果第 i 行的军人数量少于第 j 行，或者两行军人数量相同但 i 小于 j，那么我们认为第 i 行的战斗力比第 j 行弱。
+ *
+ * 军人 总是 排在一行中的靠前位置，也就是说 1 总是出现在 0 之前。
+ *
+ * 示例 1：
+ * 输入：mat =
+ * [[1,1,0,0,0],
+ *  [1,1,1,1,0],
+ *  [1,0,0,0,0],
+ *  [1,1,0,0,0],
+ *  [1,1,1,1,1]],
+ * k = 3
+ * 输出：[2,0,3]
+ * 解释：
+ * 每行中的军人数目：
+ * 行 0 -> 2
+ * 行 1 -> 4
+ * 行 2 -> 1
+ * 行 3 -> 2
+ * 行 4 -> 5
+ * 从最弱到最强对这些行排序后得到 [2,0,3,1,4]
+ *
+ * 示例 2：
+ * 输入：mat =
+ * [[1,0,0,0],
+ *  [1,1,1,1],
+ *  [1,0,0,0],
+ *  [1,0,0,0]],
+ * k = 2
+ * 输出：[0,2]
+ * 解释：
+ * 每行中的军人数目：
+ * 行 0 -> 1
+ * 行 1 -> 4
+ * 行 2 -> 1
+ * 行 3 -> 1
+ * 从最弱到最强对这些行排序后得到 [0,2,3,1]
+ *
+ * 提示：
+ * m == mat.length
+ * n == mat[i].length
+ * 2 <= n, m <= 100
+ * 1 <= k <= m
+ * */
+const kWeakestRows = (mat, k) => {
+    let len = mat.length;
+    let map = new Map(), res = [];
+    for (let i = 0; i < len; i++) {
+        const x = mat[i].filter(item => item === 1).length;
+        map.set(i, x)
+    }
+    const arr = Array.from(map.values()).sort((a, b) => a - b)
+    for (let i = 0; i < arr.length; i++) {
+        map.forEach(function(value, k) {
+            if (value === arr[i] && res.indexOf(k) === -1) {
+                res.push(k)
+            }
+        });
+    }
+    return res.slice(0, k)
+}
+// console.log(kWeakestRows([[1,1,0,0,0],[1,1,1,1,0],[1,0,0,0,0],[1,1,0,0,0],[1,1,1,1,1]], 3));
+// console.log(kWeakestRows([[1,0],[1,0],[1,0],[1,1]], 4));
