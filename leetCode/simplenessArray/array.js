315d20a532afdb6c6ab91562a3cffcb420625ff1
郭云亮 <1060950677@qq.com>
2023-09-19 10:20:55 +0800

    feat：leetcode：1331 - 数组序号转换

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -3868,3 +3868,63 @@ const decompressRLElist = (nums) => {
     return res;
 };
 // console.log(decompressRLElist([1,2,3,4]))
+
+/**
+ * 1331. 数组序号转换
+ *
+ * 给你一个整数数组 arr ，请你将数组中的每个元素替换为它们排序后的序号。
+ *
+ * 序号代表了一个元素有多大。序号编号的规则如下：
+ *
+ * 序号从 1 开始编号。
+ * 一个元素越大，那么序号越大。如果两个元素相等，那么它们的序号相同。
+ * 每个数字的序号都应该尽可能地小。
+ *
+ * 示例 1：
+ * 输入：arr = [40,10,20,30]
+ * 输出：[4,1,2,3]
+ * 解释：40 是最大的元素。 10 是最小的元素。 20 是第二小的数字。 30 是第三小的数字。
+ *
+ * 示例 2：
+ * 输入：arr = [100,100,100]
+ * 输出：[1,1,1]
+ * 解释：所有元素有相同的序号。
+ *
+ * 示例 3：
+ * 输入：arr = [37,12,28,9,100,56,80,5,12]
+ * 输出：[5,3,4,2,8,6,7,1,3]
+ *
+ * 提示：
+ * 0 <= arr.length <= 105
+ * -109 <= arr[i] <= 109
+ * */
+const arrayRankTransform = (arr) => {
+    // 方法1:
+    // let map = new Map();
+    // let newArr = Array.from(new Set([...arr])).sort((a, b) => a - b);
+    // for (let i = 0; i < newArr.length; i++) {
+    //     map.set(newArr[i], i + 1)
+    // }
+    // for (let i = 0; i < arr.length; i++) {
+    //     arr[i] = map.get(arr[i])
+    // }
+    // return arr;
+
+    // 方法2：
+    const nums = [...new Set(arr)].sort((a, b) => a - b);
+    const res = [];
+    for (let x of arr) {
+        console.log(x, 'sx')
+        let l = 0, r = arr.length - 1;
+        while (l < r) {
+            let mid = l + r >> 1;
+            console.log(mid, 'mid')
+            if (nums[mid] >= x) r = mid;
+            else l = mid + 1;
+        }
+        res.push(r + 1)
+    }
+    return res;
+}
+// console.log(arrayRankTransform([40, 10, 20, 30]))
+// console.log(arrayRankTransform([37,12,28,9,100,56,80,5,12]))
