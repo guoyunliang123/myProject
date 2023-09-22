ea7b9be70d5f2e679270fcc0aa96386edeae2a16
郭云亮 <1060950677@qq.com>
2023-09-22 15:12:15 +0800

    feat：leetcode：1437 - 是否所有 1 都至少相隔 k 个元素

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -4744,3 +4744,64 @@ const kidsWithCandies = (candies, extraCandies) => {
     return res;
 };
 // console.log(kidsWithCandies([2,3,5,1,3], 3));
+
+/**
+ * 1437. 是否所有 1 都至少相隔 k 个元素
+ *
+ * 给你一个由若干 0 和 1 组成的数组 nums 以及整数 k。如果所有 1 都至少相隔 k 个元素，则返回 True ；否则，返回 False 。
+ *
+ * 示例 1：
+ * 输入：nums = [1,0,0,0,1,0,0,1], k = 2
+ * 输出：true
+ * 解释：每个 1 都至少相隔 2 个元素。
+ *
+ * 示例 2：
+ * 输入：nums = [1,0,0,1,0,1], k = 2
+ * 输出：false
+ * 解释：第二个 1 和第三个 1 之间只隔了 1 个元素。
+ *
+ * 示例 3：
+ * 输入：nums = [1,1,1,1,1], k = 0
+ * 输出：true
+ *
+ * 示例 4：
+ * 输入：nums = [0,1,0,1], k = 1
+ * 输出：true
+ *
+ * 提示：
+ * 1 <= nums.length <= 10^5
+ * 0 <= k <= nums.length
+ * nums[i] 的值为 0 或 1
+ * */
+const kLengthApart = (nums, k) => {
+    // 方法1：
+    // let map = new Map();
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (nums[i] === 1) {
+    //         map.set(i, 1)
+    //     }
+    // }
+    // let arr = Array.from(map.keys());
+    // for (let i = 1; i < arr.length; i++) {
+    //     if (arr[i] - arr[i - 1] <= k) {
+    //         return false
+    //     }
+    // }
+    // return true;
+
+    //  方法2：
+    let idx = null;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 1) {
+            if (idx !== null && idx !== i) {
+                if (i - idx <= k) {
+                    return false;
+                }
+            }
+            idx = i;
+        }
+    }
+    return true;
+};
+// console.log(kLengthApart([1,0,0,0,1,0,0,1], 2));
+// console.log(kLengthApart([1,0,0,1,0,1], 2));
