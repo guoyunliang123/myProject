1cc2da2eb323b73ef82ef33ee920f22c9ae77b45
郭云亮 <1060950677@qq.com>
2023-09-22 10:26:21 +0800

    feat：leetcode：1408 - 数组中的字符串匹配

--- leetCode/simplenessArray/array.js
+++ leetCode/simplenessArray/array.js
@@ -4587,3 +4587,60 @@ const minSubsequence = (nums) => {
     }
 };
 // console.log(minSubsequence([4,3,10,9,8]));
+
+/**
+ * 1408. 数组中的字符串匹配
+ *
+ * 给你一个字符串数组 words ，数组中的每个字符串都可以看作是一个单词。请你按 任意 顺序返回 words 中是其他单词的子字符串的所有单词。
+ *
+ * 如果你可以删除 words[j] 最左侧和/或最右侧的若干字符得到 words[i] ，那么字符串 words[i] 就是 words[j] 的一个子字符串。
+ *
+ * 示例 1：
+ * 输入：words = ["mass","as","hero","superhero"]
+ * 输出：["as","hero"]
+ * 解释："as" 是 "mass" 的子字符串，"hero" 是 "superhero" 的子字符串。
+ * ["hero","as"] 也是有效的答案。
+ *
+ * 示例 2：
+ * 输入：words = ["leetcode","et","code"]
+ * 输出：["et","code"]
+ * 解释："et" 和 "code" 都是 "leetcode" 的子字符串。
+ *
+ * 示例 3：
+ * 输入：words = ["blue","green","bu"]
+ * 输出：[]
+ *
+ * 提示：
+ * 1 <= words.length <= 100
+ * 1 <= words[i].length <= 30
+ * words[i] 仅包含小写英文字母。
+ * 题目数据 保证 每个 words[i] 都是独一无二的。
+ * */
+const stringMatching = (words) => {
+    // 方法1：
+    // let res = [], len = words.length;
+    // words.sort((a, b) => a.length - b.length)
+    // for (let i = 0; i < len; i++) {
+    //     for (let j = len - 1; j > i; j--) {
+    //         if (words[j].includes(words[i])) {
+    //             res.push(words[i])
+    //             break; // 需要加终止当前循环，因为一个字字符串可能同时出现在不同的单词中
+    //         }
+    //     }
+    // }
+    // return res;
+
+    // 方法2：
+    // const s = words.join(','), arr = [];
+    // console.log(s)
+    // for(const word of words) {
+    //     console.log(s.indexOf(word), '111')
+    //     console.log(s.lastIndexOf(word), '222')
+    //     if(s.indexOf(word) !== s.lastIndexOf(word)){ // 题目数据 保证 每个 words[i] 都是独一无二的。
+    //         arr.push((word))
+    //     }
+    // }
+    // return arr
+};
+// console.log(stringMatching(["mass","as","hero","superhero"]));
+// console.log(stringMatching(["leetcoder","leetcode","od","hamlet","am"]));
