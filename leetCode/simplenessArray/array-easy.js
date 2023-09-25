5dfabd061c648aabe01435d6b711f7e0cbe32ccb
郭云亮 <1060950677@qq.com>
2023-09-25 10:02:36 +0800

    feat：leetcode：1464 - 数组中两元素的最大乘积

--- leetCode/simplenessArray/array-easy.js
+++ leetCode/simplenessArray/array-easy.js
@@ -4856,7 +4856,7 @@ const busyStudent = (startTime, endTime, queryTime) => {
     };
     return count;
 };
-console.log(busyStudent([1,2,3], [3,2,7], 4));
+// console.log(busyStudent([1,2,3], [3,2,7], 4));
 
 /**
  * 1460. 通过翻转子数组使两个数组相等
@@ -4896,4 +4896,48 @@ const canBeEqual = (target, arr) => {
     arr.sort((a, b) => a - b);
     return target.toString() === arr.toString();
 };
-console.log(canBeEqual([1, 2, 3, 4], [2, 4, 1, 3]))
+// console.log(canBeEqual([1, 2, 3, 4], [2, 4, 1, 3]))
+
+/**
+ * 1464. 数组中两元素的最大乘积
+ *
+ * 给你一个整数数组 nums，请你选择数组的两个不同下标 i 和 j，使 (nums[i]-1)*(nums[j]-1) 取得最大值。
+ *
+ * 请你计算并返回该式的最大值。
+ *
+ * 示例 1：
+ * 输入：nums = [3,4,5,2]
+ * 输出：12
+ * 解释：如果选择下标 i=1 和 j=2（下标从 0 开始），则可以获得最大值，(nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12 。
+ *
+ * 示例 2：
+ * 输入：nums = [1,5,4,5]
+ * 输出：16
+ * 解释：选择下标 i=1 和 j=3（下标从 0 开始），则可以获得最大值 (5-1)*(5-1) = 16 。
+ *
+ * 示例 3：
+ * 输入：nums = [3,7]
+ * 输出：12
+ *
+ * 提示：
+ * 2 <= nums.length <= 500
+ * 1 <= nums[i] <= 10^3
+ * */
+const maxProduct = (nums) => {
+    // 方法1：
+    // nums.sort((a, b) => b - a);
+    // return (nums[0] - 1) * (nums[1] - 1);
+
+    // 方法2：
+    let max = -1, second = -1;
+    for (const n of nums) {
+        if (n > max) {
+            second = max;
+            max = n;
+        } else if (n > second) {
+            second = n;
+        }
+    }
+    return (max - 1) * (second - 1);
+};
+console.log(maxProduct([3, 4, 5, 2]));
