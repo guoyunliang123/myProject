8c8a694425233e3b312efc4cd65f580ba55b23cd
郭云亮 <1060950677@qq.com>
2023-09-26 10:12:30 +0800

    feat：leetcode：1275 - 找出井字棋的获胜者

--- leetCode/simplenessArray/array-easy.js
+++ leetCode/simplenessArray/array-easy.js
@@ -3712,10 +3712,115 @@ const minTimeToVisitAllPoints = (points) => {
     for (let i = 1; i < points.length; i++) {
         let [x, y] = points[i];
         res += Math.max(Math.abs(x - points[i - 1][0]), Math.abs(y - points[i - 1][1]))
-    };
+    }
+    ;
     return res;
 };
 // console.log(minTimeToVisitAllPoints([[1,1],[3,4],[-1,0]]))
+
+/**
+ * 1275. 找出井字棋的获胜者
+ *
+ * A 和 B 在一个 3 x 3 的网格上玩井字棋。
+ *
+ * 井字棋游戏的规则如下：
+ *
+ * 玩家轮流将棋子放在空方格 (" ") 上。
+ * 第一个玩家 A 总是用 "X" 作为棋子，而第二个玩家 B 总是用 "O" 作为棋子。
+ * "X" 和 "O" 只能放在空方格中，而不能放在已经被占用的方格上。
+ * 只要有 3 个相同的（非空）棋子排成一条直线（行、列、对角线）时，游戏结束。
+ * 如果所有方块都放满棋子（不为空），游戏也会结束。
+ * 游戏结束后，棋子无法再进行任何移动。
+ * 给你一个数组 moves，其中每个元素是大小为 2 的另一个数组（元素分别对应网格的行和列），它按照 A 和 B 的行动顺序（先 A 后 B）记录了两人各自的棋子位置。
+ *
+ * 如果游戏存在获胜者（A 或 B），就返回该游戏的获胜者；如果游戏以平局结束，则返回 "Draw"；如果仍会有行动（游戏未结束），则返回 "Pending"。
+ *
+ * 你可以假设 moves 都 有效（遵循井字棋规则），网格最初是空的，A 将先行动。
+ *
+ * 示例 1：
+ * 输入：moves = [[0,0],[2,0],[1,1],[2,1],[2,2]]
+ * 输出："A"
+ * 解释："A" 获胜，他总是先走。
+ * "X  "    "X  "    "X  "    "X  "    "X  "
+ * "   " -> "   " -> " X " -> " X " -> " X "
+ * "   "    "O  "    "O  "    "OO "    "OOX"
+ *
+ * 示例 2：
+ * 输入：moves = [[0,0],[1,1],[0,1],[0,2],[1,0],[2,0]]
+ * 输出："B"
+ * 解释："B" 获胜。
+ * "X  "    "X  "    "XX "    "XXO"    "XXO"    "XXO"
+ * "   " -> " O " -> " O " -> " O " -> "XO " -> "XO "
+ * "   "    "   "    "   "    "   "    "   "    "O  "
+ *
+ * 示例 3：
+ * 输入：moves = [[0,0],[1,1],[2,0],[1,0],[1,2],[2,1],[0,1],[0,2],[2,2]]
+ * 输出："Draw"
+ * 输出：由于没有办法再行动，游戏以平局结束。
+ * "XXO"
+ * "OOX"
+ * "XOX"
+ *
+ * 示例 4：
+ * 输入：moves = [[0,0],[1,1]]
+ * 输出："Pending"
+ * 解释：游戏还没有结束。
+ * "X  "
+ * " O "
+ * "   "
+ *
+ * 提示：
+ * 1 <= moves.length <= 9
+ * moves[i].length == 2
+ * 0 <= moves[i][j] <= 2
+ * moves 里没有重复的元素。
+ * moves 遵循井字棋的规则。
+ * */
+const tictactoe = (moves) => {
+    const arr = new Array(3).fill(0).map(() => new Array(3).fill(0));
+    for (let i = 0; i < moves.length; i++) {
+        if (i % 2 === 0) {
+            arr[moves[i][0]][moves[i][1]] = 'X'
+        } else {
+            arr[moves[i][0]][moves[i][1]] = 'O'
+        }
+    }
+    console.log(arr, 'arr')
+    const arr1 = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            arr1.push(arr[i][j])
+        }
+    }
+    console.log(arr1, 'arr1')
+    let count = 0;
+    const X = [], O = [];
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] === 'X') {
+            X.push(i + 1);
+            count++;
+        } else if (arr1[i] === 'O') {
+            O.push(i + 1);
+            count++;
+        }
+    }
+    console.log(X, 'X')
+    console.log(O, 'O')
+    let V = ['123', '456', '789', '147', '258', '369', '159', '357'];
+    for (let n of V) {
+        if (X.join('').includes(n[0]) && X.join('').includes(n[1]) && X.join('').includes(n[2])) {
+            return 'A'
+        } else if (O.join('').includes(n[0])&&O.join('').includes(n[1])&&O.join('').includes(n[2])) {
+            return 'B'
+        }
+    }
+    if (count === 9) {
+        return 'Draw'
+    } else {
+        return 'Pending'
+    }
+};
+// console.log(tictactoe([[0, 0], [2, 0], [1, 1], [2, 1], [2, 2]]))
 
 /**
  * 1287. 有序数组中出现次数超过25%的元素
@@ -4581,7 +4686,7 @@ const findLucky = (arr) => {
     arr.forEach(v => hash[v] = (hash[v] || 0) + 1)
     let a = Object.keys(hash).filter(v => hash[v] === v)
     a.sort((b, c) => c - b)
-    if(a[0] === 'undefined') {
+    if (a[0] === 'undefined') {
         return -1
     }
     return a[0];
@@ -4623,7 +4728,7 @@ const minSubsequence = (nums) => {
     let count = 0;
     for (let i = 0; i < nums.length; i++) {
         count += nums[i];
-        if (count > sum - count){
+        if (count > sum - count) {
             return nums.slice(0, i + 1);
         }
     }
@@ -4895,7 +5000,8 @@ const busyStudent = (startTime, endTime, queryTime) => {
         if (queryTime >= startTime[i] && queryTime <= endTime[i]) {
             count++
         }
-    };
+    }
+    ;
     return count;
 };
 // console.log(busyStudent([1,2,3], [3,2,7], 4));
