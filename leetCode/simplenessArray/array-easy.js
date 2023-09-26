9ff2ab3aaa7b5631a0c9bc0cfb2ce4a37e2f0db9
郭云亮 <1060950677@qq.com>
2023-09-26 10:34:47 +0800

    feat：leetcode：1502 - 判断是否形成等差数列

--- leetCode/simplenessArray/array-easy.js
+++ leetCode/simplenessArray/array-easy.js
@@ -5271,3 +5271,46 @@ const average = (salary) => {
     // return (sum - salary[0] - salary[salary.length - 1]) / (salary.length - 2);
 };
 // console.log(average([8000,9000,2000,3000,6000,1000]))
+
+/**
+ * 1502. 判断能否形成等差数列
+ *
+ * 给你一个数字数组 arr 。
+ *
+ * 如果一个数列中，任意相邻两项的差总等于同一个常数，那么这个数列就称为 等差数列 。
+ *
+ * 如果可以重新排列数组形成等差数列，请返回 true ；否则，返回 false 。
+ *
+ * 示例 1：
+ * 输入：arr = [3,5,1]
+ * 输出：true
+ * 解释：对数组重新排序得到 [1,3,5] 或者 [5,3,1] ，任意相邻两项的差分别为 2 或 -2 ，可以形成等差数列。
+ *
+ * 示例 2：
+ * 输入：arr = [1,2,4]
+ * 输出：false
+ * 解释：无法通过重新排序得到等差数列。
+ *
+ * 提示：
+ * 2 <= arr.length <= 1000
+ * -10^6 <= arr[i] <= 10^6
+ * */
+const canMakeArithmeticProgression = (arr) => {
+    // 方法1：
+    // arr.sort((a, b) => a - b);
+    // let d = arr[1] - arr[0];
+    // for (let i = 2; i < arr.length; i++) {
+    //     if (arr[i] - arr[i - 1] !== d) return false;
+    // }
+    // return true;
+
+    // 方法2：
+    arr.sort((a, b) => a - b);
+    for (let i = 1, n = arr.length; i < n - 1; i++) {
+        if (arr[i] * 2 !== arr[i - 1] + arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+};
+// console.log(canMakeArithmeticProgression([1, 3, 5]))
