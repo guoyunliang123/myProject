f0101a08c6e98895a9e6774b5a18e8c008667558
郭云亮 <1060950677@qq.com>
2023-09-26 16:37:20 +0800

    feat：leetcode：1528 - 重新排列字符串

--- leetCode/simplenessArray/array-easy.js
+++ leetCode/simplenessArray/array-easy.js
@@ -5383,3 +5383,50 @@ const numIdenticalPairs = (nums) => {
 };
 // console.log(numIdenticalPairs([1, 2, 3, 1, 1, 3]))
 // console.log(numIdenticalPairs([1,1,1,1]))
+
+/**
+ * 1528. 重新排列字符串
+ *
+ * 给你一个字符串 s 和一个 长度相同 的整数数组 indices 。
+ *
+ * 请你重新排列字符串 s ，其中第 i 个字符需要移动到 indices[i] 指示的位置。
+ *
+ * 返回重新排列后的字符串。
+ *
+ * 示例 1：
+ * 输入：s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+ * 输出："leetcode"
+ * 解释：如图所示，"codeleet" 重新排列后变为 "leetcode" 。
+ *
+ * 示例 2：
+ * 输入：s = "abc", indices = [0,1,2]
+ * 输出："abc"
+ * 解释：重新排列后，每个字符都还留在原来的位置上。
+ *
+ * 提示：
+ * s.length == indices.length == n
+ * 1 <= n <= 100
+ * s 仅包含小写英文字母
+ * 0 <= indices[i] < n
+ * indices 的所有的值都是 唯一 的
+ * */
+const restoreString = (s, indices) => {
+    // 方法1：
+    // let map = new Map();
+    // for (let i = 0; i < indices.length; i++) {
+    //     map.set(indices[i], s[i])
+    // };
+    // let res = Array.from(indices.length).fill(0);
+    // for (let i = 0; i < indices.length; i++) {
+    //     res[i] = map.get(i)
+    // }
+    // return res.join('');
+
+    // 方法2：
+    let ary = [];
+    for (let i = 0; i < indices.length; i++) {
+        ary[indices[i]] = s.charAt(i)
+    };
+    return ary.join();
+};
+// console.log(restoreString("codeleet", [4, 5, 6, 7, 0, 2, 1, 3]));
