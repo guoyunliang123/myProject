0511481727081e2d7944557eaebdbd4d67e720d0
郭云亮 <1060950677@qq.com>
2023-10-08 11:01:48 +0800

    feat：leetcode：1550 - 存在连续三个奇数的数组

--- leetCode/simplenessArray/array-easy.js
+++ leetCode/simplenessArray/array-easy.js
@@ -5541,3 +5541,40 @@ const findKthPositive = (arr, k) => {
 };
 // console.log(findKthPositive([2, 3, 4, 7, 11], 5))
 // console.log(findKthPositive([7, 8, 9, 10], 5))
+
+/**
+ * 1550. 存在连续三个奇数的数组
+ *
+ * 给你一个整数数组 arr，请你判断数组中是否存在连续三个元素都是奇数的情况：如果存在，请返回 true ；否则，返回 false 。
+ *
+ * 示例 1：
+ * 输入：arr = [2,6,4,1]
+ * 输出：false
+ * 解释：不存在连续三个元素都是奇数的情况。
+ *
+ * 示例 2：
+ * 输入：arr = [1,2,34,3,4,5,7,23,12]
+ * 输出：true
+ * 解释：存在连续三个元素都是奇数的情况，即 [5,7,23] 。
+ *
+ * 提示：
+ * 1 <= arr.length <= 1000
+ * 1 <= arr[i] <= 1000
+ * */
+const threeConsecutiveOdds = (arr) => {
+    const len = arr.length;
+    if (len < 3) return false;
+    let count = 0;
+    for (let i = 0; i < len; i++) {
+        if (arr[i] % 2 === 1) {
+            count++
+            if (count === 3) return true;
+        } else {
+            count = 0;
+        }
+    }
+    return false;
+};
+console.log(threeConsecutiveOdds([1,1,1]))
+console.log(threeConsecutiveOdds([2,6,4,1]))
+console.log(threeConsecutiveOdds([1,2,34,3,4,5,7,23,12]))
