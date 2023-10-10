128b61e4fa31bdf8059e94a7a5a97e318cb073da
郭云亮 <1060950677@qq.com>
2023-10-10 16:36:06 +0800

    feat：leetcode：1566. - 重复至少 K 次且长度为 M 的模式

--- leetCode/simplenessArray/array-easy.js
+++ leetCode/simplenessArray/array-easy.js
@@ -3810,7 +3810,7 @@ const tictactoe = (moves) => {
     for (let n of V) {
         if (X.join('').includes(n[0]) && X.join('').includes(n[1]) && X.join('').includes(n[2])) {
             return 'A'
-        } else if (O.join('').includes(n[0])&&O.join('').includes(n[1])&&O.join('').includes(n[2])) {
+        } else if (O.join('').includes(n[0]) && O.join('').includes(n[1]) && O.join('').includes(n[2])) {
             return 'B'
         }
     }
@@ -5426,7 +5426,8 @@ const restoreString = (s, indices) => {
     let ary = [];
     for (let i = 0; i < indices.length; i++) {
         ary[indices[i]] = s.charAt(i)
-    };
+    }
+    ;
     return ary.join();
 };
 // console.log(restoreString("codeleet", [4, 5, 6, 7, 0, 2, 1, 3]));
@@ -5575,6 +5576,65 @@ const threeConsecutiveOdds = (arr) => {
     }
     return false;
 };
-console.log(threeConsecutiveOdds([1,1,1]))
-console.log(threeConsecutiveOdds([2,6,4,1]))
-console.log(threeConsecutiveOdds([1,2,34,3,4,5,7,23,12]))
+// console.log(threeConsecutiveOdds([1,1,1]))
+// console.log(threeConsecutiveOdds([2,6,4,1]))
+
+/**
+ * 1566. 重复至少 K 次且长度为 M 的模式
+ *
+ * 给你一个正整数数组 arr，请你找出一个长度为 m 且在数组中至少重复 k 次的模式。
+ *
+ * 模式 是由一个或多个值组成的子数组（连续的子序列），连续 重复多次但 不重叠 。 模式由其长度和重复次数定义。
+ *
+ * 如果数组中存在至少重复 k 次且长度为 m 的模式，则返回 true ，否则返回  false 。
+ *
+ * 示例 1：
+ * 输入：arr = [1,2,4,4,4,4], m = 1, k = 3
+ * 输出：true
+ * 解释：模式 (4) 的长度为 1 ，且连续重复 4 次。注意，模式可以重复 k 次或更多次，但不能少于 k 次。
+ *
+ * 示例 2：
+ * 输入：arr = [1,2,1,2,1,1,1,3], m = 2, k = 2
+ * 输出：true
+ * 解释：模式 (1,2) 长度为 2 ，且连续重复 2 次。另一个符合题意的模式是 (2,1) ，同样重复 2 次。
+ *
+ * 示例 3：
+ * 输入：arr = [1,2,1,2,1,3], m = 2, k = 3
+ * 输出：false
+ * 解释：模式 (1,2) 长度为 2 ，但是只连续重复 2 次。不存在长度为 2 且至少重复 3 次的模式。
+ *
+ * 示例 4：
+ * 输入：arr = [1,2,3,1,2], m = 2, k = 2
+ * 输出：false
+ * 解释：模式 (1,2) 出现 2 次但并不连续，所以不能算作连续重复 2 次。
+ *
+ * 示例 5：
+ * 输入：arr = [2,2,2,2], m = 2, k = 3
+ * 输出：false
+ * 解释：长度为 2 的模式只有 (2,2) ，但是只连续重复 2 次。注意，不能计算重叠的重复次数。
+ *
+ * 提示：
+ * 2 <= arr.length <= 100
+ * 1 <= arr[i] <= 100
+ * 1 <= m <= 100
+ * 2 <= k <= 100
+ * */
+const containsPattern = (arr, m, k) => {
+    if (m * k > arr.length) return false;
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === arr[i + m]) {
+            count++
+            // 第一次出现重复的时候是计算两次，随后每当 count 的次数增加 m 的时候，实则只出现一次重复
+            if (Math.floor(count / m) + 1 === k) {
+                return true
+            }
+        } else {
+            count = 0
+        }
+    }
+    return false;
+}
+// console.log(containsPattern([1, 2, 1, 2, 1, 1, 1, 3], 2, 2))
+// console.log(containsPattern([1,2,1,2,1,3], 2, 3))
+// console.log(containsPattern([1,2,4,4,4,4], 1, 3))
