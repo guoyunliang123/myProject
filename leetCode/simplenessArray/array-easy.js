6fa2519f3c3b02a4e0ef3effee3f341016a4b802
郭云亮 <1060950677@qq.com>
2023-10-11 10:53:02 +0800

    feat：leetcode：1572 - 矩阵对角线元素的和

--- leetCode/simplenessArray/array-easy.js
+++ leetCode/simplenessArray/array-easy.js
@@ -5638,3 +5638,59 @@ const containsPattern = (arr, m, k) => {
 // console.log(containsPattern([1, 2, 1, 2, 1, 1, 1, 3], 2, 2))
 // console.log(containsPattern([1,2,1,2,1,3], 2, 3))
 // console.log(containsPattern([1,2,4,4,4,4], 1, 3))
+
+/**
+ * 1572. 矩阵对角线元素的和
+ *
+ * 给你一个正方形矩阵 mat，请你返回矩阵对角线元素的和。
+ *
+ * 请你返回在矩阵主对角线上的元素和副对角线上且不在主对角线上元素的和。
+ *
+ * 示例  1：
+ * 输入：mat = [[1,2,3],
+ *             [4,5,6],
+ *             [7,8,9]]
+ * 输出：25
+ * 解释：对角线的和为：1 + 5 + 9 + 3 + 7 = 25
+ * 请注意，元素 mat[1][1] = 5 只会被计算一次。
+ *
+ * 示例  2：
+ * 输入：mat = [[1,1,1,1],
+ *             [1,1,1,1],
+ *             [1,1,1,1],
+ *             [1,1,1,1]]
+ * 输出：8
+ *
+ * 示例 3：
+ * 输入：mat = [[5]]
+ * 输出：5
+ *
+ * 提示：
+ * n == mat.length == mat[i].length
+ * 1 <= n <= 100
+ * 1 <= mat[i][j] <= 100
+ * */
+const  diagonalSum = (mat) => {
+    // 方法1：
+    // let len = mat.length;
+    // let count = 0;
+    // for (let i = 0; i < len; i++) {
+    //     count += mat[i][i]
+    //     count += mat[i][len - 1 - i]
+    // }
+    // if (len % 2 === 1) {
+    //     let x = Math.floor(len / 2)
+    //     count -= mat[x][x]
+    // }
+    // return count;
+
+    // 方法2：
+    const n = mat.length;
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+        sum += mat[i][i] + mat[i][n - 1 - i];
+        if (i === n - 1- i) sum -= mat[i][i];
+    }
+    return sum;
+};
+console.log(diagonalSum([[1,2,3],[4,5,6],[7,8,9]]))
